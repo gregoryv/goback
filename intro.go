7ed66fe5cc2ccfb2297b3f2fa3f2a7387c7dd2fe
grevin <gregory.vincic@assaabloy.com>
2024-03-28 08:57:27 +0100

    History

--- intro.go
+++ intro.go
@@ -20,18 +20,30 @@ func introSection(p *Deck) {
 		"margin: auto auto",
 		"width: 70%",
 	)
+	p.Style("pre.small",
+		"font-size: 17px",
+	)
+	p.Style(".people",
+		"margin-top: 2em",
+		"margin-left: 1em",
+		"float: right",
+	)
 	p.NewCard(
 		H2("History"),
 
-		Div(Class("narrow"),
+		Middle(70,
+
+			Img(Class("people"), Src("img/people.png")),
+
 			P(`Robert Griesemer, Ken Thompson and Rob Pike are the
-		original authors of the Go language which is currently
-		supervised by Russ Cox. Ian wrote the first compiler as a
-		frontend to gcc.`),
-		),
-		TwoCol(
-			Center(Div(Class("small"),
-				Pre(`The story
+		    original authors of the Go language which is currently
+		    supervised by Russ Cox. Ian wrote the first compiler as a
+		    frontend to gcc.`),
+
+			Center(
+
+				Br(),
+				Pre(Class("small"), `The story
 
   1960       1970          1980          1990        2000      2007-2009
 
@@ -48,17 +60,6 @@ func introSection(p *Deck) {
               Pascal               +---- Oberon             ---+
 `),
 			),
-			),
-
-			Wrap(
-				Br(),
-
-				Img(Src("img/people.png")),
-
-				P(`They needed a language for writing servers on
-				modern multicore computers`),
-			),
-			40,
 		),
 	)
 	p.NewCard(
@@ -91,7 +92,7 @@ go version go1.22.0 linux/amd64`,
 		"padding-right: 1em",
 	)
 	p.Style(".structures td *",
-		"font-size: 0.7em",
+		"font-size: 0.6em",
 	)
 	p.Style(".structures, td.wide",
 		"width: 100%",
@@ -100,33 +101,30 @@ go version go1.22.0 linux/amd64`,
 	p.NewCard(
 		H2("Project structure"),
 
-		Middle(80,
-
-			Table(Class("structures wide"),
-				Tr(
-					Td(
-						Shell("$ cd neon; tree/", "examples/neon.tree"),
-						quote(
-							"A basic Go package has all its code in the project’s root directory.",
-							"https://go.dev/doc/modules/layout",
-						),
+		Table(Class("structures wide"),
+			Tr(
+				Td(
+					Shell("$ cd neon; tree/", "examples/neon.tree"),
+					quote(
+						"A basic Go package has all its code in the project’s root directory.",
+						"https://go.dev/doc/modules/layout",
 					),
-					Td(
-						Shell("$ cd neon/; tree", "examples/neon2.tree"),
-					),
-					Td(
-						Shell("$ cd neon/; tree", "examples/neon3.tree"),
+				),
+				Td(
+					Shell("$ cd neon/; tree", "examples/neon2.tree"),
+				),
+				Td(
+					Shell("$ cd neon/; tree", "examples/neon3.tree"),
 
-						quote("Server project",
-							"https://go.dev/doc/modules/layout#server-project",
-						),
+					quote("Server project",
+						"https://go.dev/doc/modules/layout#server-project",
 					),
-					Td(Class("wide"),
-						Shell("$ cd neon/; tree", "examples/neon4.tree"),
+				),
+				Td(Class("wide"),
+					Shell("$ cd neon/; tree", "examples/neon4.tree"),
 
-						quote("Don't waste a good name",
-							"Gregory Vinčić",
-						),
+					quote("Don't waste a good name",
+						"Gregory Vinčić",
 					),
 				),
 			),
